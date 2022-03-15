db482f0844f0ae09a7bbd4ea32eb85a4a2c77f35
averagephotographer <christopher.tullier98@gmail.com>
2022-03-15 16:45:35 -0500

    cyber

---
+++ 1 Cryptography/.history/scytale_20220314114146.py
@@ -0,0 +1,12 @@
+message = "some random message"
+jump = 4
+new = ""
+i = 0
+
+msg_len = len(message)
+
+while i < msg_len*4:
+    new += message[i%msg_len]
+    i += jump
+
+print(new)
---
+++ 1 Cryptography/.history/scytale_20220314120850.py
@@ -0,0 +1,16 @@
+message = "some random message"
+jump = 4
+new = ""
+i = 0
+
+msg_len = len(message)
+
+while i < msg_len*4:
+    new += message[i%msg_len]
+    i += jump
+
+print(new)
+
+
+# Anky's cipher
+# echo "worklaptop" | tr a-z n-za-m
