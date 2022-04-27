de21fd002e7eb146a3cc1db3a2d90706f1c66e6e
averagephotographer <christopher.tullier98@gmail.com>
2022-04-27 12:18:34 -0400

    logger

--- 7 Keystroke Dynamics/logger.py
+++ 7 Keystroke Dynamics/logger.py
@@ -1,13 +1,23 @@
 from pynput.keyboard import Key, Listener
+# for linux
+from termios import tcflush, TCIFLUSH
+from sys import stdin, stdout
+
 
 def on_press(key):
-    print(f"{key} was pressed")
+    try:
+        print(key.char)
+        # print(key.char.encode("ascii"))
+    # print(f"{key} was pressed")
+    except AttributeError:
+        print(str(key))
 
 def on_release(key):
-    print(f"{key} was released")
+    # print(f"{key} was released")
     if (key == Key.esc):
         return False
 
 with Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
     
+tcflush(stdin, TCIFLUSH) # flush my stdin buffer
